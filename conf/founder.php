bddd394e19e12a1000b093f0a6427c772e28094a
test <test@example.com>
2024-03-23 14:04:38 +0800

    fix err

---
+++ conf/founder.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 
+ * @author lindong <lindong@moresec.cn> 2015-11-2
+ * @link http://172.16.7.200:80
+ * @license
+ */
+  return array(
+	'admin' => '21232f297a57a5a743894a0e4a801fc3',
+);
+?>
